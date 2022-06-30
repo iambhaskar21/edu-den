839c2734a9fbe08e76df46fb1a09cf4ee637f66c
TrailblaZer132 <uditjeet@gmail.com>
2022-07-01 01:12:04 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { Navbar } from "./Components/Navbar/Navbar";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -6,9 +11,23 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="navbar" element={<Navbar />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
         </Routes>
       </Router>
+
+      {/* <Route path="/" element={<Navbar />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
