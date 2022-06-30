afe4de2cfdd3b0557203c46738df48a51110ac0a
iambhaskar21 <iambhaskar21@gmail.com>
2022-07-01 03:11:22 +0530

    navbar ui ( 0 )

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,42 @@
 import React from 'react'
-import { NavbarElements, NavbarHeader, NavbarMain } from './NavbarStyle'
-
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
+import { NavbarElements, NavbarHeader, NavbarMain, NavBarLink, ClickIcon } from './NavbarStyle'
+import { AboutUs } from '../Pages/About/AboutUs'
+import { NoPage } from '../Pages/NoPage/NoPage'
+import { Home } from '../Pages/Home/Home'
+import { ContactUs } from '../Pages/Contact/ContactUs'
+import './NavStyle.css'
 export const Navbar = () => {
   return (
     <>
+   
+
+
+    <BrowserRouter>
     <NavbarMain>
         <NavbarHeader>
             Header
         </NavbarHeader>
-
         <NavbarElements>
-            <div>
-                ITem 1
-            </div>
-            <div>
-                Item 2
-            </div>
-            <div>
-                Item 3
-            </div>
+         
+          <NavBarLink to='/' exact activeClassName="active" > Home</NavBarLink>
+          <NavBarLink to='/about'  activeClassName="active"> About</NavBarLink>
+          <NavBarLink to='/contact' activeClassName="active"> Contact</NavBarLink>
         </NavbarElements>
+        <ClickIcon/>
     </NavbarMain>
+   
+
+      <Routes>
+       
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="*" element={<NoPage />} />
+          
+        
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/Components/Navbar/NavbarStyle.js
+++ src/Components/Navbar/NavbarStyle.js
@@ -1,29 +1,58 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import {BrowserRouter as Router, NavLink } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa'
+
+
 
 export const NavbarMain = styled.div`
     background: #5ba56e;
+    display:none;
     display: flex;
     height: 8vh;
     width: 100%;
     justify-content: space-between;
     align-items: center;
     z-index: 1;
-    position: fixed;
+    position: relative;
+    margin-top: 0px;
+    margin-right: auto;
+    margin-bottom: 0px;
+    margin-left: auto;
     color: white;
+    
 `;
 
 export const NavbarHeader = styled.div`
     display: block;
     padding-left:2em;
     font-size:xx-large;
+    cursor: pointer;
     
 `
 
 export const NavbarElements = styled.div`
     display: flex;
     float: right;
-    width: 50vw;
+    width: 30vw;
     justify-content: space-around;
-    background-color: black;
+    
 
+`
+export const NavBarLink= styled(NavLink)`
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+    @media (max-width: 768px) {
+    display:none;
+  }
+    
+`
+export const ClickIcon=styled(FaBars)`
+    font-size: 2em;
+    color: white;
+    padding-right:1em;
+    @media (min-width:768px) {
+        display: none;
+    }
+   
 `
